cfea4e6625669c9522d4261175c9ddf9e422492c
zbcwise <zbcwise@ZBC-E-RO-23261>
2022-09-06 21:09:49 +0200

    Add project files.

---
+++ Grid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+enum Ships
+{
+    hangarship,
+    battleship,
+    destroyer,
+    uboat,
+    scoutboat
+}
+namespace BattleShips
+{
+    internal class Grid
+    {
+        private bool occupied;
+
+        public bool Occupied
+        {
+            get { return occupied; }
+            set { occupied = value; }
+        }
+
+        private Ships occupiedWith;
+
+        public Ships OccupiedWith
+        {
+            get { return occupiedWith; }
+            set { occupiedWith = value; }
+        }
+
+
+
+        private bool hit;
+
+        public bool Hit
+        {
+            get { return hit; }
+            set { hit = value; }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿namespace BattleShips
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Grid[,] playerGrid = new Grid[10, 10];
+            Grid[,] enemyGrid = new Grid[10, 10];
+            for (int i = 0; i < playerGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < playerGrid.GetLength(1); j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("\n");
+            }
+            Console.SetBufferSize(150, 150);
+            program.ChooseShip();
+            program.CursorMover();
+        }
+        void ChooseShip()
+        {
+            string[] ships = { "HangarShip", "BattleShip", "Destroyer", "Uboat", "Scoutboat" };
+            Console.SetCursorPosition(12, 0);
+            foreach (string item in ships)
+            {
+                Console.WriteLine(item);
+                Console.CursorLeft = 12;
+            }
+            int i = 0;
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (i < ships.Length - 1)
+                        {
+                            i++;
+                            Console.SetCursorPosition(ships[i].Length, i);
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (i > 0)
+                        {
+                            i--;
+                            Console.SetCursorPosition(ships[i].Length, i);
+                        }
+                        break;
+                }
+
+        }
+        void CursorMover()
+        {
+            int down = 0;
+            int left = 0;
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if(down > 0)
+                        down--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        down++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if(left > 0)
+                        left--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        left++;
+                        break;
+                }
+                Console.SetCursorPosition(left, down);
+            }
+        }
+    }
+}
